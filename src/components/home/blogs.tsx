d9e71010e371da0e2493163c7550d60e777625b8
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-16 22:49:30 +0800

    feat: edited blogs

--- src/components/home/blogs.tsx
+++ src/components/home/blogs.tsx
@@ -8,28 +8,15 @@ import courseData from "@/data/cards_data.json";
 function Blogs() {
   return (
     <div id="highlight">
-      <div className="min-h-screen py-12 pt-36">
-        <h1 className="text-4xl md:text-5xl text-center font-sans font-extrabold mb-8 text-black">
+      <div className="min-h-screen   mx-10 py-12">
+        <h1 className="text-2xl mb-6 md:text-5xl text-center font-sans font-extrabold  text-[#af3753]">
           News & Blogs
         </h1>
         <div className="flex flex-wrap justify-center">
           {courseData.courses.map((course) => (
             <CardContainer className="inter-var m-4" key={course.slug}>
-              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                <CardItem
-                  translateZ="50"
-                  className="text-xl font-bold text-white"
-                >
-                  {course.title}
-                </CardItem>
-                <CardItem
-                  as="p"
-                  translateZ="60"
-                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                >
-                  {course.description}
-                </CardItem>
-                <CardItem translateZ="100" className="w-full mt-4">
+              <CardBody className="bg-[#FFD1DC] relative group/card  hover:shadow-2xl shadow-emerald-500/[0.1]  border-[#af3753]/[02] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                <CardItem translateZ="100" className="w-full mt-1">
                   <Image
                     src={course.image}
                     height="1000"
@@ -37,6 +24,19 @@ function Blogs() {
                     className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                     alt="thumbnail"
                   />
+                </CardItem>
+                <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-[#af3753]"
+                >
+                  {course.title}
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-[#af3753] text-sm max-w-sm mt-2"
+                >
+                  {course.description}
                 </CardItem>
               </CardBody>
             </CardContainer>
