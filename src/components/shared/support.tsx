09349f6ea9c4ce2f251e6c6dc0b6859ace7cbc58
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-18 12:19:49 +0800

    feat: fix text

--- src/components/shared/support.tsx
+++ src/components/shared/support.tsx
@@ -5,20 +5,22 @@ import Link from "next/link";
 export default function Support() {
   return (
     <section className="relative z-[2] bg-[#9B5DE5]">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-9 lg:gap-18 container pt-20 lg:pt-40">
-        <div className="order-last lg:order-first max-lg:max-h-96 overflow-hidden">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-9 md:gap-18 p-10 pb-0 md:p-0 container ">
+        <div className="order-last md:order-first overflow-hidden">
           <Image
             src="/support.jpg"
             title="support"
             alt="support"
-            width={674}
-            height={522}
-            className="w-full max-w-full overflow-hidden"
+            width={500}
+            height={500}
+            className="overflow-hidden"
           />
         </div>
-        <div className="text-white flex flex-col gap-7">
-          <div className="h2-gap max-w-md">
-            <h2>Support Our Mission Today!</h2>
+        <div className="text-white flex items-center gap-7">
+          <div className="max-w-md">
+            <h2 className="text-secondary-50 font-sans font-extrabold mb-3 text-2xl md:text-5xl">
+              Support Our Mission Today!
+            </h2>
             <p>
               Join our cause to transform structures around reproductive health
               and family well-being.
